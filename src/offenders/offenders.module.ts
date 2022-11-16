25393ac4c9d10128415450e07e9055a3abdb75c3
Felipe Marques <contato@felipe.marques.com>
2022-11-16 17:56:42 -0300

    creation of the inner join of all tables

--- src/offenders/offenders.module.ts
+++ src/offenders/offenders.module.ts
@@ -1,11 +1,22 @@
-import { ratingReviews  } from '@prisma/client';
+import { ratingReviews } from '@prisma/client';
 import { registerEnumType } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
 import { OffendersService } from './offenders.service';
 import { OffendersResolver } from './offenders.resolver';
+import { AddressesResolver } from 'src/addresses/addresses.resolver';
+import { AddressesService } from 'src/addresses/addresses.service';
+import { ComplementariesResolver } from 'src/complementaries/complementaries.resolver';
+import { ComplementariesService } from 'src/complementaries/complementaries.service';
 
 @Module({
-  providers: [OffendersResolver, OffendersService]
+  providers: [
+    OffendersResolver,
+    OffendersService,
+    AddressesResolver,
+    AddressesService,
+    ComplementariesResolver,
+    ComplementariesService,
+  ],
 })
 export class OffendersModule {
   constructor() {
--- src/offenders/offenders.resolver.ts
+++ src/offenders/offenders.resolver.ts
@@ -1,12 +1,34 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, ResolveField, Parent } from '@nestjs/graphql';
 import { OffendersService } from './offenders.service';
 import { Offender } from './entities/offender.entity';
 import { CreateOffenderInput } from './dto/create-offender.input';
 import { UpdateOffenderInput } from './dto/update-offender.input';
+import { Address } from 'src/addresses/entities/address.entity';
+import { AddressesService } from 'src/addresses/addresses.service';
+import { Complementary } from 'src/complementaries/entities/complementary.entity';
+import { ComplementariesService } from 'src/complementaries/complementaries.service';
 
 @Resolver(() => Offender)
 export class OffendersResolver {
-  constructor(private readonly offendersService: OffendersService) {}
+  constructor(
+    private readonly offendersService: OffendersService,
+    private readonly addressesService: AddressesService,
+    private readonly complementariesService: ComplementariesService,
+  ) {}
+
+  @ResolveField('address', () => Address)
+  async address(@Parent() offender: Offender) {
+    if (offender.address_uid) {
+      return this.addressesService.findOne(offender.address_uid);
+    }
+  }
+
+  @ResolveField('complementaries', () => Complementary)
+  async complementary(@Parent() offender: Offender) {
+    if (offender.complement_uid) {
+      return this.complementariesService.findOne(offender.complement_uid);
+    }
+  }
 
   @Mutation(() => Offender)
   createOffender(@Args('createOffenderInput') createOffenderInput: CreateOffenderInput) {
